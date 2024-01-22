a3e5a243419b01107c3dad18339d72b65f8b883d
chandru00 <45612602+chandru00@users.noreply.github.com>
2024-01-22 01:19:40 -0500

    remove unused library

--- src/main/java/dev/chan/chancontentcalendar/ChanContentCalendarApplication.java
+++ src/main/java/dev/chan/chancontentcalendar/ChanContentCalendarApplication.java
@@ -1,21 +1,10 @@
 package dev.chan.chancontentcalendar;
 
-import java.time.LocalDateTime;
-
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 import dev.chan.chancontentcalendar.config.ContentCalendarProperties;
-import dev.chan.chancontentcalendar.model.Content;
-import dev.chan.chancontentcalendar.model.Status;
-import dev.chan.chancontentcalendar.model.Type;
-import dev.chan.chancontentcalendar.repository.ContentRepository;
 
 @EnableConfigurationProperties(ContentCalendarProperties.class)
 @SpringBootApplication
